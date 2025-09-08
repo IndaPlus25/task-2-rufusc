b5cc1d7a220b8cea91c895aed26560d423f38600
Rufus Chow <rufuschow@BarbardhorRufus.localdomain>
2025-09-08 23:20:28 +0200

    Solution for for summera tal #1

---
+++ summera_tal/src/main.rs
@@ -0,0 +1,30 @@
+use std::{io};
+use std::io::prelude::*;
+
+fn main() {
+    let input = io::stdin();
+
+    let lines = input
+        .lock()
+        .lines()
+        .map(|_line| _line.ok().unwrap())
+        .collect::<Vec<String>>();
+
+    let vector_length = (lines[0].parse::<usize>().unwrap() + 1) / 2;
+
+    let mut vector_numbers: Vec<i32> = lines[1]
+        .split_whitespace()
+        .map(|number_string|number_string.parse::<i32>().unwrap())
+        .collect();
+    
+    vector_numbers.sort();
+    
+    let amount: i32 = vector_numbers.iter()
+        .rev()
+        .take(vector_length)
+        .sum();
+
+    eprintln!("Kattis print this comment to an irrelevant stream!");
+    println!("{amount}");
+
+}
