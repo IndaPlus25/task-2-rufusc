f8928cdd2f2c457bede37a1b28d275a39b3ee119
Rufus Chow <rufuschow@BarbardhorRufus.localdomain>
2025-09-09 17:09:46 +0200

    new image for summera tal and solution for avstand till kanten

--- avstand_till_kanten/src/main.rs
+++ avstand_till_kanten/src/main.rs
@@ -1,3 +1,33 @@
+use std::cmp::min;
+use std::io;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let input = io::stdin();
+
+    let mut lines = input
+        .lock()
+        .lines()
+        .map(|_line| _line.ok().unwrap())
+        .collect::<Vec<String>>();
+
+    let rectangle_dimensions: Vec<i32> = lines[0]
+        .split_whitespace()
+        .map(|number_string|number_string.parse::<i32>().unwrap())
+        .collect();
+
+    let height = rectangle_dimensions[0];
+    let width = rectangle_dimensions[1];
+
+    for row in 0 .. height{
+        for column in 0 .. width {
+            let distance_to_edge = min(min(row, height - row - 1), min(column, width - column - 1)) + 1;
+            if distance_to_edge < 10 {
+                print!("{}", distance_to_edge);
+            } else {
+                print!(".")
+            }
+        }
+        println!();
+    }
 }
